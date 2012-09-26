bc1aea0f780eec50fc2f6d4e6487445100a98f63
Epskampie <simon@frontwise>
2012-09-26 09:32:00 +0200

    Improved documentation

--- directories.js
+++ directories.js
@@ -1,6 +1,6 @@
 var directories = [
   // Put the directories you want to watch here.
-  // Dont forget to use forward slashed / or escaped backslashes \\\\
+  // Dont forget to use forward slashed / or escaped backslashes \\
   // Uncomment the next lines for an example
 
   //'C:\\Users\\Simon\\Documents\\MyProject1',
